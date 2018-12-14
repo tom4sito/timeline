e75d13a85fcbe0708924fd5e179402e63ab5aa0b
ctorres <ctorres@addc.net>
2018-12-14 15:15:49 -0500

    second commit

---
+++ js/Responsive-Animated-Timeline/README.md
@@ -0,0 +1,13 @@
+# TimeLine
+
+Simple elegant plugin time line slider.
+
+![DEMO](https://github.com/AhmedBHameed/TimeLine/blob/master/img/DEMO.gif)
+
+# Getting started
+
+This plugin used with Jquery library so make sure to attache this plugin after importing jquery to your project.
+
+# Licenses
+
+This is MIT software licenses. Feel free to use it.
---
+++ test.php
@@ -0,0 +1,485 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Arnouse Digital Devices - Pluggable Computing Platform - BioDigitalPC&reg;</title>
+<meta name="keywords" content="Arnouse Digital Devices ADDC BioDigital BioDigitalPC credit-card PC microcomputer minicomputer microPC miniPC" />
+<meta name="description" content="Arnouse Digital Devices, Corp. BioDigitalPC Ecosystem">
+<meta name="author" content="addc.net">
+<link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,700,600' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="//addc.net/img/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="//addc.net/img/apple-touch-icon.png">
+<meta name="theme-color" content="#003070">
+<meta name="msapplication-navbutton-color" content="#003070">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link rel="stylesheet" href="//addc.net/css/addc.min.css">
+<link rel="stylesheet" href="//addc.net/css/admin.css?v=8"> 
+<style type="text/css">
+
+	.addc-history-thumb{
+		max-height:100%;
+		max-width:100%;
+	}
+	
+	#aboutUsHeader{
+		background-image: url(/img/headers/about_us_header.jpg);
+	}
+
+	p{
+/*		font-size: 30px;
+		color: #000;*/
+	}
+
+	.wrap {
+     width: 95%;
+     min-width: 900px;
+	}
+
+	#timelinecontainer{
+		overflow-y: hidden;
+	}
+
+	#maincont {
+		width: 160px;
+	}
+
+	.year-head {
+		cursor: pointer;
+	}
+
+	.year-head-active {
+		color:#1a6fb9;
+		font-size: 60px;
+	}
+
+	.generic{
+		color: #000;
+		font-size: 30px;
+	}
+
+	.generic-active{
+		color: #1a6fb9;
+		font-size: 30px;
+	}
+
+	@media screen and (max-width: 420px) {
+	  .yearimg {
+	    width: 130px;
+	  }
+	  #year-content{
+	  	padding-top: 80px;
+	  }
+	}
+
+	.yearimg {
+		/*display:none;*/
+	}
+		
+</style>
+</head>
+<body>
+<div class="body">
+<header id="header" class="header-narrow header-semi-transparent" data-plugin-options='{"stickyEnabled": true, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": true, "stickyStartAt": 1, "stickySetTop": "1"}'>
+<div class="header-body">
+<div class="header-container container">
+<div class="header-row">
+<div class="header-column">
+<div class="header-logo">
+<a href="/">
+<img alt="ADDC" src="/img/logo-default-slim-dark.png">
+</a>
+</div>
+</div>
+<div class="header-column">
+<div class="header-row">
+<div class="header-nav header-nav-stripe">
+<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main">
+<i class="fa fa-bars"></i>
+</button>
+<div class="header-nav-main header-nav-main-square header-nav-main-effect-2 header-nav-main-sub-effect-1 collapse">
+<nav>
+<ul class="nav nav-pills" id="mainNav">
+<li class="dropdown dropdown-mega">
+<a class="dropdown-toggle" href="#">Products</a>
+<ul class="dropdown-menu">
+<li>
+<div class="dropdown-mega-content">
+<div class="row">
+<div class="col-md-3">
+<ul class="dropdown-mega-sub-nav">
+<li><a style="font-size:1.1em;font-weight:600;" href="/products"><i style="color:black;" class="fa fa-fw fa-arrow-circle-o-right"></i>VIEW PRODUCT LIST</a></li>
+</ul>
+<span class="divider"></span>
+<span class="dropdown-mega-sub-title">Cards</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/biodigitalpc-11">BioDigitalPC<sup>&reg;</sup> 11</a></li>
+<li><a href="/products/biodigitalpc-10">BioDigitalPC<sup>&reg;</sup> 10</a></li>
+<li><a href="/products/biodigitalpc-9">BioDigitalPC<sup>&reg;</sup> 9</a></li>
+<li><a href="/products/biodigitalpc-8">BioDigitalPC<sup>&reg;</sup> 8</a></li>
+<li><a href="/products/older-biodigitalpcs">Earlier Generations</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<span class="dropdown-mega-sub-title">Docks</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/cruiserone">CruiserOne</a></li>
+<li><a href="/products/desktop-dock-3">Desktop Dock 3.0</a></li>
+<li><a href="/products/desktop-dock-2">Desktop Dock 2.0</a></li>
+<li><a href="/products/desktop-dock-1">Desktop Dock 1.0</a></li>
+<li><a href="/products/quick-connect">Quick Connect</a></li>
+<li><a href="/products/podcom-line">PodCom Line</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<span class="dropdown-mega-sub-title">Mobyl Data Centers</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/mdc-8">Mobyl Data Center 8: MDC-8</a></li>
+<li><a href="/products/mdc-10">Mobyl Data Center 10: MDC-10</a></li>
+<li><a href="/products/mdc-sr10">Mobyl Data Center SR10: MDC-SR10</a></li>
+<li><a href="/products/mdc-22">Mobyl Data Center 22: MDC-22</a></li>
+</ul>
+<span class="dropdown-mega-sub-title">Data Center</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/sr-60">SR-60</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<span class="dropdown-mega-sub-title">Third Party Docks</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/cis-phone">CIS Polycom Phone</a></li>
+<li><a href="/products/all-in-one/">All-in-One Monitor</a></li>
+<span class="dropdown-mega-sub-title">Software</span>
+<ul class="dropdown-mega-sub-nav">
+<li><a href="/products/romware-plus">ROMware+</a></li>
+</ul>
+<li class="divider"></li>
+<li><a href="PRICE_LIST" target="_blank">Price List</a></li>
+</ul>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</li>
+<li><a href="/products/integration">Integration</a></li>
+<li class="dropdown">
+<a class="dropdown-toggle" href="#">Support</a>
+<ul class="dropdown-menu">
+<li><a href="/support">Support</a></li>
+<li><a href="/support/documentation">Installation Guides &amp; Documentation</a></li>
+<li><a href="/support/drivers">Drivers</a></li>
+<li><a href="/support/product-sheets">Product Sheets</a></li>
+<li><a href="/support/supported-os">Supported Operating Systems</a></li>
+<li><a href="/support/marketing-material">Marketing Material</a></li>
+<li><a href="/support/3d-models">3D Models</a></li>
+<li><a href="/faq">FAQ</a></li>
+<li><a href="/support/clients-port">Client Support</a></li>
+</ul>
+</li>
+<li class="dropdown">
+<a class="dropdown-toggle" href="#">Use Cases</a>
+<ul class="dropdown-menu">
+<li><a href="/use-cases/third-party-tests">Third Party Tests</a></li>
+<li><a href="/use-cases/military">Military &amp; Government</a></li>
+<li><a href="/use-cases/healthcare">Healthcare</a></li>
+<li><a href="/use-cases/law-enforcement">Public Safety/Law Enforcement</a></li>
+<li><a href="/use-cases/small-business">Small &amp; Midsize Business</a></li>
+</ul>
+</li>
+<li class="dropdown">
+<a class="dropdown-toggle" href="#">Partners</a>
+<ul class="dropdown-menu">
+<li><a href="/partners/global-partners">Global Partners</a></li>
+<li><a href="/partners/oem-partners">OEMs</a></li>
+<li><a href="/partners/resellers">Resellers</a></li>
+<li><a href="/partners/clients">Clients</a></li>
+</ul>
+</li>
+<li class="dropdown">
+<a class="dropdown-toggle" href="#">ABOUT US</a>
+<ul class="dropdown-menu">
+<li><a href="/about">About Us</a></li>
+<li><a href="/news">News</a></li>
+<li><a href="/blog">Blog</a></li>
+<li><a href="/patents">Our Patents</a></li>
+</ul>
+</li>
+<li><a href="/contact">Contact Us</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</header><section class="page-header">
+<div class="container">
+<div style="height:89px;">
+&nbsp;
+</div>
+</div>
+</section>
+<div id="aboutUsHeader" class="container-fluid sub-header-row">
+<div class="container">
+<h1 class="header-title">About <span style="font-weight:600;">Us</span></h1>
+</div>
+</div>
+<div class="container">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding:0;">
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<p class="lead" style="font-size:2em;line-height:1em;margin:20px 10px 40px 10px;color:black;">
+Arnouse Digital Devices, Corp., (ADDC) is a high tech computer manufacturer, concentrating on the miniaturization, and small form-factor, of high-performance x86 pluggable computers, servers and high security devices. We proudly make all our products in the USA.
+</p>
+</div>
+</div>
+<hr class="short">
+<h1 style="font-size:2em;" class="titillium-light">Who <span class="titillium-semi-bold">We</span> Are</h1>
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<div style="font-size:1.1em;margin:0 10px 10px 10px;">
+<p>Arnouse Digital Devices Corporation (ADDC) is a privately held company.</p>
+<p>It was founded in 2002 by Michael Arnouse to create a low powered interchangeable compute platform allowing users an unprecedented level of flexibility with ultra-high security in a portable micro form factor.</p>
+<p>ADDC has been granted 18 utility patents, with three pending in the areas of pluggable computing and its surrounding ecosystem, which cover all aspects of ADDC products and technology.</p>
+<p>ADDC is fully committed to providing the world's best computing solutions for fixed and mobile environments enabling consumers, governments and businesses to excel. ADDC products are proudly created and made in the USA.</p>
+</div>
+</div>
+</div>
+<hr class="short">
+<h1 style="font-size:3em;text-transform:none;">Our <span class="titillium-semi-bold">History</span></h1>
+<div class="row">
+<!-- ----------------------------------------------------------------------------------------------------------- -->
+	<div id="timelinecontainer" class="row" style="margin-left: 10px;">
+		<div id="maincont" class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+			<p id="ele1" class="year-head" tracker="0">2001</p>
+			<p id="ele2" class="year-head" tracker="1">2002</p>
+			<p id="ele3" class="year-head" tracker="2">2004</p>
+			<p id="ele4" class="year-head" tracker="3">2008</p>
+			<p id="ele5" class="year-head" tracker="4">2010</p>
+			<p id="ele6" class="year-head" tracker="5">2011</p>
+			<p id="ele7" class="year-head" tracker="6">2013</p>
+			<p id="ele7" class="year-head" tracker="7">2015</p>
+		</div>
+
+		<div id="year-content" class="col-xs-6 col-sm-8 col-md-10 col-lg-10" >
+			<img class="yearimg" src="" width="260" heigth="404">
+		</div>
+
+	</div>
+</div>
+</div>
+</div>
+<div class="modal fade" id="newsletterModal" tabindex="-1" role="dialog" aria-labelledby="newsletterModalBody" aria-hidden="true">
+<div class="modal-dialog modal-sm">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+<h4 class="modal-title" id="newsletterModalTitle"></h4>
+</div>
+<div class="modal-body" id="newsletterModalBody">
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+<footer id="footer">
+<div class="container">
+<div class="row">
+<div class="footer-ribbon">
+<span class="font-bold">GET IN TOUCH</span>
+</div>
+<div class="col-xs-12 col-md-3">
+<div class="newsletter mt-lg">
+<h4>Newsletter</h4>
+<p>Keep up on our always evolving product features and technology. Enter your e-mail and subscribe to our newsletter.</p>
+<div class="input-group">
+<input class="form-control" placeholder="Email Address" id="footerEmail" name="newsletterEmail" type="text">
+<span class="input-group-btn">
+<button id="footerGo" class="btn btn-default" type="submit" style="background-color: #2a3890;">Go!</button>
+</span>
+</div>
+</div>
+</div>
+<div class="col-xs-12 col-md-4 col-md-offset-2">
+<div class="contact-details mt-lg">
+<h4>Contact Us</h4>
+<ul class="contact">
+<li><p><strong>Address:</strong>&nbsp;1983 Marcus Ave. Suite 104<br> Lake Success, NY 11042</p></li>
+<li><p><strong>Phone:</strong>&nbsp;516.673.4444</p></li>
+<li><p><strong>Email:</strong>&nbsp;<a href="/cdn-cgi/l/email-protection#3a494f4a4a55484e7a5b5e5e5914545f4e"><span class="__cf_email__" data-cfemail="02717772726d707642636666612c6c6776">[email&#160;protected]</span></a></p></li>
+</ul>
+</div>
+</div>
+<div class="col-xs-12 col-md-3" style="position:relative;float:right;">
+<div class="mt-lg" style="padding-right:15px;">
+<div style="width:147px;text-align:left;">
+<h4>Follow Us</h4>
+</div>
+<ul class="list-inline">
+<li><a href="https://www.facebook.com/ArnouseDigitalDevices/" target="_blank" title="Facebook"><i class="fa fa-4x fa-facebook-square"></i></a></li>
+<li><a href="https://twitter.com/arnouse_digital/" target="_blank" title="Twitter"><i class="fa fa-4x fa-twitter-square"></i></a></li>
+<li><a href="https://www.instagram.com/arnouse_digital/" target="_blank" title="Instagram"><i class="fa fa-4x fa-instagram"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-copyright">
+<div class="container">
+<div class="row">
+<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 hidden-xs hidden-sm">
+<a href="index.html" class="logo">
+<img alt="ADDC" style="max-width:100%;" src="/img/logo.png">
+</a>
+</div>
+<div class="col-xs-7 col-sm-7 col-md-5 col-lg-5">
+<p>&copy; Copyright ADDC 2016. All Rights Reserved.</p>
+</div>
+<div class="col-xs-5 col-sm-5 col-md-6 col-lg-6 text-right">
+<nav id="sub-menu" style="font-size:0.9em;padding-right:5px;">
+<ul>
+<li><a href="/privacy-and-terms/">Legal</a></li>
+<li><a href="/faq">FAQ</a></li>
+<li><a href="/sitemap">Sitemap</a></li>
+<li><a href="/contact">Contact</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</footer> </div>
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="//addc.net/js/addc.min.js"></script>
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-43974301-1', 'auto');ga('send', 'pageview');</script>
+<script type="text/javascript">
+	var yearTracker = -1;
+	var scrollDirection = 0;
+	var lastYearClicked = "";
+
+	var browserType = browserVersion();
+	var browserData = browserType.split(" ");
+
+	var yearImgArray = ["img0.png", "img1.png", "img2.png", "img3.png", "img4.jpg", "img5.jpg", "img6.png", "img7.jpg"];
+
+	if(browserData[0].toLowerCase() == "firefox" && (parseInt(browserData[1]) > 30))
+	{
+		console.log("firefox: " + browserData[1]);
+		scrollFirefox();
+	}
+	else{
+		console.log("not firefox");
+		scrollNotFirefox();
+	}
+
+	function browserVersion(){
+		var ua= navigator.userAgent, tem, 
+	    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	    if(/trident/i.test(M[1])){
+	        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+	        return 'IE '+(tem[1] || '');
+	    }
+	    if(M[1]=== 'Chrome'){
+	        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+	        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+	    }
+	    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+	    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+	    return M.join(' ');
+	}
+
+	function scrollFirefox(){
+		document.getElementById("maincont").addEventListener("DOMMouseScroll", function(e){
+			console.log(e.detail);
+			if(e.detail > scrollDirection){
+				if(yearTracker < 7){
+					yearTracker += 1;
+					var currElement = this.children[yearTracker];
+					$(currElement).css({"color":"red", "font-size": "60px"});
+					$(currElement).prev().css({"color":"black", "font-size": "30px"});
+				}			
+			}
+			else{
+				if(yearTracker > 0){
+					yearTracker -= 1;
+					var currElement = this.children[yearTracker];
+					$(currElement).css({"color":"red", "font-size": "60px"});
+					$(currElement).prev().css({"color":"black", "font-size": "30px"});
+					$(currElement).next().css({"color":"black", "font-size": "30px"});	
+				}			
+			}
+			console.log(yearTracker);
+		});
+	}
+
+	function scrollNotFirefox(){
+		document.getElementById("timelinecontainer").addEventListener("wheel", function(e){
+			var maincont = this.children[0];
+			var wheeldata = e.wheelDelta * -1;
+			if(wheeldata > scrollDirection){
+				if(yearTracker < 7){
+					yearTracker += 1;
+					var currElement = maincont.children[yearTracker];
+					$(currElement).addClass("year-head-active");
+					$(currElement).removeClass("generic");
+					lastYearClicked = currElement;
+					$(currElement).prev().removeClass("year-head-active");
+					// $(".yearimg").hide(function(){
+					// 	$(".yearimg").attr("src", "images/" + yearImgArray[yearTracker]);
+					// }).fadeIn();
+					$(".yearimg").attr("src", "images/" + yearImgArray[yearTracker]);
+					
+				}			
+			}
+			else{
+				if(yearTracker > 0){
+					yearTracker -= 1;
+					var currElement = maincont.children[yearTracker];
+					$(currElement).removeClass("generic");
+					$(currElement).addClass("year-head-active");
+					$(currElement).prev().removeClass("year-head-active");
+					$(currElement).next().removeClass("year-head-active");
+					lastYearClicked = currElement;
+					$(".yearimg").attr("src", "images/" + yearImgArray[yearTracker]);
+				}			
+			}
+			console.log(yearTracker);
+			event.preventDefault();
+		});
+	}
+
+// mouse events ----------------------------------
+	$(".year-head").on("mouseover", function(){
+		if(!$(this).hasClass("year-head-active")){
+			$(this).removeClass("generic");
+			$(this).addClass("generic-active");
+		}
+	});
+
+	$(".year-head").on("mouseout", function(){
+		if(!$(this).hasClass("year-head-active")){
+			$(this).removeClass("generic-active");
+		}	
+	});
+
+	$(".year-head").on("click", function(){
+		$(this).removeClass("generic-active");
+		$(this).removeClass("generic");
+		$(lastYearClicked).removeClass("year-head-active");
+		lastYearClicked = $(this);
+		$(this).addClass("year-head-active");
+		yearTracker = parseInt($(this).attr("tracker"));
+		$(".yearimg").attr("src", "images/" + yearImgArray[yearTracker]);
+	});
+
+</script>
+</body>
+</html>
